5061a46a1dba6e19524b74704066c648abe748a7
moutasem <bumoutasem@gmail.com>
2023-10-16 10:23:20 +0300

    last last

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -7,7 +7,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 import { CommonModule } from '@app/common';
 @Module({
   imports: [
--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SalesService } from './services/sales.service';
 import { SalesController } from './services/sales.controller';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
   providers: [SalesService],
