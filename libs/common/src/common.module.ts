f90ba034a83e68b7fc12a4be8a7fa14421dcaeb6
moutasem <bumoutasem@gmail.com>
2023-10-23 08:47:49 +0300

    hot

--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { IsExistConstraint } from './validator/IsExist';
 import { DatabaseModule } from '../../../src/database/database.module';
+import { UniqueConstraint } from './validator/Unique';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [IsExistConstraint, IsExistConstraint, CommonService],
-  exports: [IsExistConstraint, IsExistConstraint, CommonService],
+  providers: [IsExistConstraint, UniqueConstraint, CommonService],
+  exports: [IsExistConstraint, UniqueConstraint, CommonService],
 })
 export class CommonModule {}
